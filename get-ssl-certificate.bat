7575ee1bc67ad79a9de4aeb0e31585ab758bc230
Steven Harradine <stevenharradine@gmail.com>
2021-03-26 10:33:32 -0400

    fix comment; fix redirect to windows NUL

--- get-ssl-certificate.bat
+++ get-ssl-certificate.bat
@@ -1,4 +1,4 @@
-# Source: https://serverfault.com/questions/661978/displaying-a-remote-ssl-certificate-details-using-cli-tools @ 20170616 18:30UTC
+REM Source: https://serverfault.com/questions/661978/displaying-a-remote-ssl-certificate-details-using-cli-tools @ 20170616 18:30UTC
 @echo off
 set site=%1
-openssl s_client -showcerts -servername %site% -connect %site%:443 2>/dev/null | openssl x509 -inform pem -noout -text
+openssl s_client -showcerts -servername %site% -connect %site%:443 2> NUL | openssl x509 -inform pem -noout -text
