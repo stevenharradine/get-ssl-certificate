2be792cdf5d82540f0cfbebc41a2bd06192769ba
Steven Harradine <stevenharradine@gmail.com>
2021-03-26 10:35:58 -0400

    echo off must be first

--- get-ssl-certificate.bat
+++ get-ssl-certificate.bat
@@ -1,4 +1,5 @@
-REM Source: https://serverfault.com/questions/661978/displaying-a-remote-ssl-certificate-details-using-cli-tools @ 20170616 18:30UTC
 @echo off
+REM Source: https://serverfault.com/questions/661978/displaying-a-remote-ssl-certificate-details-using-cli-tools @ 20170616 18:30UTC
+
 set site=%1
 openssl s_client -showcerts -servername %site% -connect %site%:443 2> NUL | openssl x509 -inform pem -noout -text
